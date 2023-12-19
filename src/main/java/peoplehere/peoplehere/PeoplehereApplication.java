2b8de6c70c2f4e720bd21641a51a2875e69b0d4f
DDongYul <comeme0101@naver.com>
2023-12-19 15:41:09 +0900

    initial commit

---
+++ src/main/java/peoplehere/peoplehere/PeoplehereApplication.java
@@ -0,0 +1,13 @@
+package peoplehere.peoplehere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplehereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplehereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/peoplehere/peoplehere/PeoplehereApplicationTests.java
@@ -0,0 +1,13 @@
+package peoplehere.peoplehere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplehereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
