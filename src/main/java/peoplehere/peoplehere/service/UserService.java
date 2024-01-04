fa37cd9bb1fdb6bc2855afd92635eee3ad4db4fd
DDongYul <comeme0101@naver.com>
2024-01-04 22:27:15 +0900

    feat: 유저 서비스 초안

--- src/main/java/peoplehere/peoplehere/service/UserService.java
+++ src/main/java/peoplehere/peoplehere/service/UserService.java
@@ -4,7 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import peoplehere.peoplehere.controller.dto.user.GetUserResponse;
+import peoplehere.peoplehere.controller.dto.user.PostUserRequest;
+import peoplehere.peoplehere.controller.dto.user.UserDtoConverter;
+import peoplehere.peoplehere.domain.Tour;
+import peoplehere.peoplehere.domain.TourHistory;
+import peoplehere.peoplehere.domain.User;
 import peoplehere.peoplehere.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -13,5 +22,86 @@ import peoplehere.peoplehere.repository.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    /**
+     * 회원 가입
+     */
+    public void createUser(PostUserRequest postUserRequest) {
+
+    }
+
+    /**
+     * 회원 탈퇴
+     */
+    public void deleteUser(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        user.setStatus("삭제"); //TODO: userStatus 코드 상수화 시키기
+    }
+
+    /**
+     * 로그인
+     */
+
+
+    /**
+     * 로그아웃
+     */
+
+
+    /**
+     * 모든 유저 조회
+     */
+    public List<GetUserResponse> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        List<GetUserResponse> getUserResponses = new ArrayList<>();
+        for (User user : users) {
+            getUserResponses.add(UserDtoConverter.userToGetUserResponse(user));
+        }
+        return getUserResponses;
+    }
+
+    /**
+     * 특정 유저 조회
+     */
+    public GetUserResponse getUser(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        return UserDtoConverter.userToGetUserResponse(user);
+    }
+
+    /**
+     * 화원 정보 수정
+     * TODO: userModifyRequest 형식 만들기
+     */
+    public void modifyUser(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+
+    }
+
+    /**
+     * 유저가 만든 투어 조회
+     */
+    public List<Tour> getCreatedTour(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        return user.getTours();
+    }
+
+    /**
+     * 유저가 이용한 투어 조회
+     */
+    public List<TourHistory> getTourHistory(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        return user.getTourHistories();
+    }
+
+
+    /**
+     * 유저 채팅 조회
+     */
+
+
+    /**
+     * 차단하기
+     */
+
 
 }
