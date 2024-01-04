d81fac5167b8b3c86c43b9890332491da20ca94e
DDongYul <comeme0101@naver.com>
2024-01-04 22:27:38 +0900

    feat: 유저 dto converter

---
+++ src/main/java/peoplehere/peoplehere/controller/dto/user/UserDtoConverter.java
@@ -0,0 +1,23 @@
+package peoplehere.peoplehere.controller.dto.user;
+
+import peoplehere.peoplehere.domain.User;
+
+public class UserDtoConverter {
+
+    /**
+     * User -> GetUserResponse
+     * 유저 -> 유저 조회 DTO
+     */
+    public static GetUserResponse userToGetUserResponse(User user) {
+        GetUserResponse getUserResponse = new GetUserResponse();
+        getUserResponse.setEmail(user.getEmail());
+        getUserResponse.setName(user.getName());
+        getUserResponse.setGender(user.getGender());
+        getUserResponse.setLeader(user.isLeader());
+        getUserResponse.setImageUrl(user.getImageUrl());
+        getUserResponse.setContent(user.getContent());
+        return getUserResponse;
+    }
+
+
+}
