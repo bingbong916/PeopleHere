e55ce60d37fa569b3960679d3113df793a847b54
DDongYul <comeme0101@naver.com>
2024-02-11 19:44:46 +0900

    feat: language 데이터 추가 - ISO국가코드 이용

--- src/main/java/peoplehere/peoplehere/domain/Language.java
+++ src/main/java/peoplehere/peoplehere/domain/Language.java
@@ -2,6 +2,7 @@ package peoplehere.peoplehere.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,19 +11,22 @@ import peoplehere.peoplehere.domain.util.BaseTimeEntity;
 
 @Entity
 @Getter @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Language extends BaseTimeEntity {
-
-    public Language(String name) {
-        this.name = name;
-    }
+public class Language {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "language_id")
     private Long id;
 
-    private String name;
+    public Language(String englishName, String koreanName, String isoCode) {
+        this.englishName = englishName;
+        this.koreanName = koreanName;
+        this.isoCode = isoCode;
+    }
 
-    @ColumnDefault("'일반'")
-    private String status = "일반";
+    private String englishName;
+
+    private String koreanName;
+
+    private String isoCode;
 
 }
