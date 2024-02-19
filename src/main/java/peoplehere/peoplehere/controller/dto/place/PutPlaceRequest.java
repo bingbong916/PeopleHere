7c2e94a087cc0216eb99ee2243fa4258f53b3797
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-02-19 20:21:49 +0900

    fix: 지역 단위 ~구, ~시로 표시되도록 수정

--- src/main/java/peoplehere/peoplehere/controller/dto/place/PutPlaceRequest.java
+++ src/main/java/peoplehere/peoplehere/controller/dto/place/PutPlaceRequest.java
@@ -4,15 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import peoplehere.peoplehere.controller.dto.image.PostImageRequest;
+
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PutPlaceRequest {
-    private String content;
-    private String imageUrl;
-    private String address;
-    private int order;
-    private Long tourId;
+    private Long placeId;
+    private String placeName;
+    private List<PostImageRequest> placeImage;
+    private String placeAddress;
+    private LatLngDto latLng;
+    private int placeOrder;
 }
